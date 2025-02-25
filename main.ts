883d3737f6215a40033ffe7bbc36335225584724
LisaVanderElst <LisaVanderelst19@gmail.com>
2025-02-25 15:03:15 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,65 @@
+let ir_waarde = 0
+IR.IR_init()
+let vooruit = 47
+let achteruit = 4
+let links = 25
+let rechts = 22
+let stop = 15
+basic.forever(function () {
+    ir_waarde = IR.IR_read()
+})
+basic.forever(function () {
+    if (ir_waarde == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (ir_waarde == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (ir_waarde == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (ir_waarde == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (ir_waarde == links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
